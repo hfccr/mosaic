634d3b90b4591d2e0e81b57b4949417a55ff8ece
hfccr <hfccr@outlook.com>
2024-02-14 08:54:16 +0530

    Add provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,29 @@
+"use client";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { WagmiProvider } from "wagmi";
+import { config } from "@/util/wagmiConfig";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Mosaic",
-  description: "ZK Trust Network",
-};
+const queryClient = new QueryClient();
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider>
+              <Header />
+              {children}
+            </RainbowKitProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
+      </body>
     </html>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,185 @@
+import * as React from "react";
+import { useState } from "react";
+import { useTheme } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import MenuItem from "@mui/material/MenuItem";
+import Link from "next/link";
+import { Connect } from "@/components/Connect";
+
+const pages = [
+  {
+    title: "Network",
+    href: "/dapp/network",
+  },
+  {
+    title: "Add To Network",
+    href: "/dapp/add",
+  },
+  {
+    title: "Confirm ",
+    href: "/dapp/confirm",
+  },
+];
+
+export default function Header() {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+  const theme = useTheme();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        elevation={0}
+        position="fixed"
+        color="inherit"
+        sx={{
+          padding: 1,
+        }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Link href="/" legacyBehavior>
+              <Box>
+                <Typography
+                  variant="h1"
+                  component="div"
+                  color="primary"
+                  sx={{
+                    cursor: "pointer",
+                    fontFamily: "Monoton, sans-serif",
+                    display: { xs: "none", sm: "none", md: "none", lg: "flex" },
+                    fontWeight: 400,
+                  }}
+                >
+                  mosaic
+                </Typography>
+                <Typography
+                  variant="h3"
+                  component="div"
+                  color="primary"
+                  sx={{
+                    cursor: "pointer",
+                    fontFamily: "Monoton, sans-serif",
+                    display: { xs: "none", md: "flex", lg: "none" },
+                  }}
+                >
+                  mosaic
+                </Typography>
+              </Box>
+            </Link>
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="app-menu"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <Link key={page.href} href={page.href} legacyBehavior>
+                    <MenuItem onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{page.title}</Typography>
+                    </MenuItem>
+                  </Link>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h5"
+              noWrap
+              component="div"
+              color="primary"
+              sx={{
+                fontFamily: "Monoton",
+                display: { xs: "none", sm: "flex", md: "none" },
+                mr: 2,
+                flexGrow: 1,
+              }}
+            >
+              mosaic
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              color="primary"
+              sx={{
+                fontFamily: "Monoton",
+                display: { xs: "flex", sm: "none" },
+                mr: 1,
+                flexGrow: 1,
+              }}
+            >
+              mosaic
+            </Typography>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: {
+                  xs: "none",
+                  md: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                },
+              }}
+            >
+              {pages.map((page) => (
+                <Link key={page.href} href={page.href} legacyBehavior>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    color="inherit"
+                    sx={{ my: 2, display: "block", mx: 0 }}
+                  >
+                    {page.title}
+                  </Button>
+                </Link>
+              ))}
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+              }}
+            >
+              <Connect />
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+  );
+}
