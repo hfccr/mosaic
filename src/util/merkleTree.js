0047446ff5cf05b51bdcd8ab018e4168a67813da
Bernardo Vieira <19441097+obernardovieira@users.noreply.github.com>
2024-02-18 01:22:55 +0000

    fix merkle tree

--- src/util/merkleTree.js
+++ src/util/merkleTree.js
@@ -1,8 +1,8 @@
-import { sha256 } from "viem";
+import { sha256, toHex } from "viem";
 import { MerkleTree } from 'merkletreejs';
 
 export function computeMerkleRoot(connections) {
-    const leaves = connections.map(x => sha256(x))
+    const leaves = connections.map(x => sha256(toHex(x)))
     const tree = new MerkleTree(leaves, sha256)
     const root = tree.getRoot()
     return root;
