2b7bdc6ab2b36a532dde54f0e2809072559da4ad
Bernardo Vieira <19441097+obernardovieira@users.noreply.github.com>
2024-02-18 01:24:54 +0000

    add friend page is now invite friend page

--- src/app/dapp/profile/AddFriend.js
+++ src/app/dapp/profile/AddFriend.js
@@ -1,24 +1,56 @@
-import { getDetailsForAddress, setDetailsForAddress } from "@/util/storage";
+import { getKVForAddress, setKVForAddress } from "@/util/storage";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import { useAccount, useSignMessage, useReadContract } from "wagmi";
+import addresses from "@/util/contractAddresses";
+import { abi } from "@/util/trustNetworkABI";
+import { computeMerkleRoot } from "@/util/merkleTree";
+import { toHex } from "viem";
 
 export default function AddFriend() {
-  useEffect(() => {
-    const getFriends = async () => {
-      const details = await getDetailsForAddress("abc");
-    };
-    getFriends();
-  }, []);
+  const account = useAccount();
+  const { data: userTrustNetwork } = useReadContract({
+    address: addresses.TrustNetwork,
+    abi,
+    functionName: "trustNetwork",
+    args: [account.address],
+  });
   const [address, setAddress] = useState("");
+  const {
+    data: signMessageData,
+    error,
+    isLoading,
+    signMessage,
+  } = useSignMessage();
+
   const onAddressChange = (e) => {
     setAddress(e.target.value);
   };
-  const add = async () => {
-    setDetailsForAddress("abc", { friends: [address] });
+  const handleAddFriend = async () => {
+    const friends = (await getKVForAddress(account.address))?.friends || [];
+    // TODO: add line below
+    // setKVForAddress(account.address, { friends: friends.concat([address]) });
+
+    const connections = friends
+      .concat([address.toLowerCase()])
+      .concat(
+        Array(9 - friends.length).fill(
+          "0x0000000000000000000000000000000000000000"
+        )
+      );
+    const rootHash = computeMerkleRoot(connections);
+
+    signMessage({
+      message: JSON.stringify({
+        previousRootHash: userTrustNetwork[0],
+        newRootHash: toHex(rootHash),
+      }),
+    });
   };
+
   return (
     <Stack spacing={2}>
-      <Typography variant="h4">Add Friend</Typography>
+      <Typography variant="h4">Invite Friend</Typography>
       <Box components="form" noValidate autoComplete="off">
         <Stack direction="row" spacing={4} alignItems="center">
           <TextField
@@ -33,12 +65,21 @@ export default function AddFriend() {
           <Button
             variant="contained"
             sx={{ flexShrink: 0 }}
-            onClick={add}
+            onClick={handleAddFriend}
             type="submit"
           >
-            Add Friend
+            Invite
           </Button>
         </Stack>
+        <Typography variant="caption" color="textSecondary">
+          {isLoading && "Loading..."}
+        </Typography>
+        {signMessageData && (
+          <Typography variant="caption" color="textSecondary">
+            Signature:
+            {signMessageData}
+          </Typography>
+        )}
       </Box>
     </Stack>
   );
